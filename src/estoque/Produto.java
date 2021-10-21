75f0b32389f87bc322e6293be6d3fd7d0b4a80dd
MedeirosRodrigo <rodrigo.medeiros30@gmail.com>
2021-10-21 20:14:40 -0300

    Criando classe produtos

--- src/estoque/Produto.java
+++ src/estoque/Produto.java
@@ -1,9 +1,38 @@
 package estoque;
 
 public class Produto {
-
-	public Produto() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
+		private int id_produto;
+		private double preco_produto;
+		private String nome_produto;
+		private int qtda_produto;
+		
+		
+		public int getId_produto() {
+			return id_produto;
+		}
+		public void setId_produto(int id_produto) {
+			this.id_produto = id_produto;
+		}
+		public double getPreco_produto() {
+			return preco_produto;
+		}
+		public void setPreco_produto(double preco_produto) {
+			this.preco_produto = preco_produto;
+		}
+		public String getNome_produto() {
+			return nome_produto;
+		}
+		public void setNome_produto(String nome_produto) {
+			this.nome_produto = nome_produto;
+		}
+		public int getQtda_produto() {
+			return qtda_produto;
+		}
+		public void setQtda_produto(int qtda_produto) {
+			this.qtda_produto = qtda_produto;
+		}	
+	
+	
+			
 }
