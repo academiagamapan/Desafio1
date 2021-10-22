1542e851de29179884a4bffe6ffcb65872b04f10
Felipe <felipecandidoa@gmail.com>
2021-10-21 21:18:27 -0300

    Subindo classe pagamento

--- src/financeiro/Pagamento.java
+++ src/financeiro/Pagamento.java
@@ -1,9 +1,20 @@
 package financeiro;
 
 public class Pagamento {
+	
 
-	public Pagamento() {
-		// TODO Auto-generated constructor stub
+
+	public static double pagamentoAVista(double valor) { // dinheiro ou pix, 20% desconto
+		double desconto;
+		desconto = valor * 0.2;
+		return valor - desconto;
+		
 	}
-
+	
+	public static double creditoAVista(double valor) { // 10% de desconto
+		double desconto;
+		desconto = valor * 0.1;
+		return valor - desconto;		
+	}
+	
 }
